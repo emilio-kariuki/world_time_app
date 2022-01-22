dd8fa085dea556cc641f991b6b6a7243c670f2d1
emilio kariuki <70817006+emilio-kariuki@users.noreply.github.com>
2022-01-22 17:20:36 +0300

    images

--- lib/pages/choose_loacation.dart
+++ lib/pages/choose_loacation.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: unused_local_variable
+
 import 'package:flutter/material.dart';
+import "package:world_time_app/services/world_time.dart";
 
 class ChooseLocation extends StatefulWidget {
   ChooseLocation({Key? key}) : super(key: key);
@@ -8,15 +11,60 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+  List<WorldTime> locations = [
+    WorldTime(url: 'Europe/London', location: 'London', flag: 'uk.png'),
+    WorldTime(url: 'Europe/Berlin', location: 'Athens', flag: 'greece.png'),
+    WorldTime(url: 'Africa/Cairo', location: 'Cairo', flag: 'egypt.png'),
+    WorldTime(url: 'Africa/Nairobi', location: 'Nairobi', flag: 'kenya.png'),
+    WorldTime(url: 'America/Chicago', location: 'Chicago', flag: 'usa.png'),
+    WorldTime(url: 'America/New_York', location: 'New York', flag: 'usa.png'),
+    WorldTime(url: 'Asia/Seoul', location: 'Seoul', flag: 'south_korea.png'),
+    WorldTime(url: 'Asia/Jakarta', location: 'Jakarta', flag: 'indonesia.png'),
+  ];
+
+  void updateTime(index) async {
+    WorldTime obj_1 = locations[index];
+    await obj_1.getTime();
+  }
+
+  String? place;
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blueGrey,
       appBar: AppBar(
-        title: Text("Loaction"),
+        title: Text("Location"),
         elevation: 0,
       ),
-      body: Text("Location choosen")
+      body: ListView.builder(
+        itemCount: locations.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.fromLTRB(5, 5, 5, 0),
+            child: Card(
+              elevation: 5,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20)),
+              child: ListTile(
+                  onTap: () {
+                    setState(() {
+                      updateTime(index);
+                    });
+                  },
+                  title: Text(locations[index].location),
+                  leading: CircleAvatar(
+                    backgroundImage:
+                        AssetImage("assets/${locations[index].flag}"),
+                  )),
+            ),
+          );
+        },
+      ),
     );
   }
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -15,42 +15,62 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     data = ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>;
-    // ignore: avoid_print
     print(data);
+
+    // setting background
+
+    // ignore: unused_local_variable
+    String bgImage = data['isDayTime'] ? "day.png" : "night.png";
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(10, 120, 0, 0),
-        child: SafeArea(
-            child: Column(
-          children: [
-            Center(
-              child: FlatButton.icon(
-                  onPressed: () {
-                    Navigator.pushNamed(context, "/location");
-                  },
-                  icon: Icon(Icons.edit_location),
-                  label: Text("Edit Location")),
-            ),
-            SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(data["location"],
-                    style: TextStyle(
-                      fontSize: 28,
-                      // fontWeight: FontWeight.bold,
-                      letterSpacing: 2.0,
-                    ))
-              ],
-            ),
-            SizedBox(height: 20),
-            Text(
-              data['time'],
-              style: TextStyle(
-                fontSize: 66
-              )),
-          ],
-        )),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/$bgImage"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(10, 120, 0, 0),
+          child: SafeArea(
+              child: Column(
+            children: [
+              Center(
+                child: FlatButton.icon(
+                    onPressed: () {
+                      Navigator.pushNamed(context, "/location");
+                    },
+                    icon: Icon(
+                      Icons.edit_location,
+                      color: Colors.grey[300],
+                      size: 10
+                      ),
+                    label: Text(
+                      "Edit Location",
+                      style: TextStyle(
+                        // fontSize: 10,
+                        color: Colors.grey[300],
+                      ),
+                      )
+                  ),
+              ),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(data["location"],
+                      style: TextStyle(
+                        fontSize: 28,
+                        // fontWeight: FontWeight.bold,
+                        letterSpacing: 2.0,
+                        color: Colors.white,
+                      ))
+                ],
+              ),
+              SizedBox(height: 10),
+              Text(data['time'], style: TextStyle(fontSize: 66,color: Colors.white)),
+            ],
+          )),
+        ),
       ),
     );
   }
